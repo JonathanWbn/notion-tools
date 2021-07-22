f55e9d3305db5f1b8b984740abce774368556405
JonathanWbn <jwieben@hey.com>
2021-07-22 17:24:37 +0200

    feat: add title and favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from 'react'
 import { UserProvider } from '@auth0/nextjs-auth0'
 import 'tailwindcss/tailwind.css'
+import Head from 'next/head'
 
 import { Header } from '../infrastructure/components/header'
 
@@ -13,6 +14,11 @@ interface Props {
 const App: FunctionComponent<Props> = ({ Component, pageProps }: Props) => {
   return (
     <UserProvider>
+      <Head>
+        <title>Notion Tools</title>
+        <link rel="shortcut icon" type="image/png" href="/favicon.png" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Header />
       <main>
         <Component {...pageProps} />
