0783db3fa035ff3eb2d0f61d442eacc857bacf22
JonathanWbn <jwieben@hey.com>
2021-07-23 20:42:22 +0200

    chore: wording

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -42,7 +42,7 @@ const User: FunctionComponent = () => {
                   href="https://api.notion.com/v1/oauth/authorize?client_id=e2305792-b84e-4d00-bffb-a026ebed4f56&redirect_uri=https://notion-tools.io/api/notion/connect&response_type=code"
                 >
                   <span className="border-b border-darkBlue text-current font-medium">
-                    Re-connect Notion
+                    Connect Notion
                   </span>
                 </a>
               </div>
