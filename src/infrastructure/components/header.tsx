039f1b8d5f4f07c4f40b02cc6e5e461cbbdf0698
JonathanWbn <jwieben@hey.com>
2021-07-23 20:50:38 +0200

    feat: add logout

--- src/infrastructure/components/header.tsx
+++ src/infrastructure/components/header.tsx
@@ -1,18 +1,27 @@
 import { ReactElement } from 'react'
 import { useUser } from '@auth0/nextjs-auth0'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export function Header(): ReactElement {
   const { user } = useUser()
+  const router = useRouter()
 
   return (
     <header className="flex justify-between p-6 items-center">
       <Link href="/">
         <a className="text-3xl">🧰</a>
       </Link>
-      {user ? (
+      {router.asPath === '/user' ? (
+        <a
+          className="border-b border-gray-500 opacity-60 hover:opacity-100"
+          href="/api/auth/logout"
+        >
+          logout
+        </a>
+      ) : user ? (
         <Link href="/user">
-          <a className="border-b border-gray-500 opacity-60 hover:opacity-100">My tools</a>
+          <a className="border-b border-gray-500 opacity-60 hover:opacity-100">my tools</a>
         </Link>
       ) : (
         <div>
