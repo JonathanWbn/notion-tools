64a912b69d4face24a02019be6a6fa4ffd779a27
JonathanWbn <jwieben@hey.com>
2021-10-08 20:57:13 +0200

    chore: add owner field

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -29,7 +29,7 @@ const User: FunctionComponent = () => {
             </p>
             <Button
               color="blue"
-              href="https://api.notion.com/v1/oauth/authorize?client_id=e2305792-b84e-4d00-bffb-a026ebed4f56&redirect_uri=https://notion-tools.io/api/notion/connect&response_type=code"
+              href="https://api.notion.com/v1/oauth/authorize?client_id=e2305792-b84e-4d00-bffb-a026ebed4f56&redirect_uri=https://notion-tools.io/api/notion/connect&response_type=code&owner=user"
             >
               {user.notionAccess ? 'Re-connect Notion' : 'Connect Notion'}
             </Button>
