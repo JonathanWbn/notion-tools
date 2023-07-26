2847930ec1711e3324d609986347cc60354a3b93
JonathanWbn <jwieben@hey.com>
2023-07-26 14:26:04 +0200

    simplify handler

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -1,18 +1,10 @@
 import { handleLogin } from '@auth0/nextjs-auth0'
 import type { NextRequest } from 'next/server'
 
-export const GET = async (req: NextRequest, ctx: { params: {} }) => {
-  try {
-    console.log('req is req', req instanceof Request)
-    console.log('req has headers', req.headers)
-    await handleLogin(req, ctx, {
-      returnTo: '/user',
-      authorizationParams: {
-        screen_hint: 'signup',
-      },
-    })
-  } catch (err) {
-    console.log('err', err)
-    return new Response(undefined, { status: 500 })
-  }
-}
+export const GET = async (req: NextRequest, ctx: { params: {} }) =>
+  handleLogin(req, ctx, {
+    returnTo: '/user',
+    authorizationParams: {
+      screen_hint: 'signup',
+    },
+  })
